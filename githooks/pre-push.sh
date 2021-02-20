89d0e7689705d21ae3063cf824f0e850b9161359
felixxfwang <felixxfwang@tencent.com>
2021-02-20 15:30:31 +0800

    update submodule by pre-commit hook

--- githooks/pre-push.sh
+++ githooks/pre-push.sh
@@ -5,11 +5,14 @@ echo 'pre-push hook:'
 checkGitStatus() {
     pwd
     status=`git status|grep 'nothing to commit'`
-    if [[ -z $status ]]; then
+    if [[ -z $status ]];
+    then
         echo "git changed"
         git add .
         git commit -m "update submodule by pre-push hook"
         git push
+    else
+        echo "git not changed"
     fi
 }
 
