2e8fefa5d54cb80e807dd35278664ec9485cfa2b
felixxfwang <felixxfwang@tencent.com>
2021-03-01 18:17:40 +0800

    add datasource

---
+++ utils/src/main/java/org/tiramisu/utils/DeviceUtil.kt
@@ -0,0 +1,16 @@
+package org.tiramisu.utils
+
+import com.blankj.utilcode.util.PhoneUtils
+
+/**
+ * @author felixxfwang
+ */
+object DeviceUtil {
+//    fun getMcc(): String {
+//
+//    }
+//
+//    fun getMnc(): String {
+//
+//    }
+}
