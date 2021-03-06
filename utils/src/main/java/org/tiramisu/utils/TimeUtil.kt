f4d2cce79a66adf79f44197f3b5371679487cbb5
felixxfwang <felixxfwang@tencent.com>
2021-03-06 15:06:24 +0800

    add timer

---
+++ utils/src/main/java/org/tiramisu/utils/TimeUtil.kt
@@ -0,0 +1,15 @@
+package org.tiramisu.utils
+
+import com.blankj.utilcode.util.TimeUtils
+
+object TimeUtil {
+
+    private const val HOUR = 60 * 60 * 1000
+
+    fun millisToStringWithHour(millis: Long): String {
+        val hour = millis / HOUR
+        val left = millis % HOUR
+        val postfix = TimeUtils.millis2String(left, "mm:ss")
+        return if (hour > 0) "$hour:$postfix" else postfix
+    }
+}
