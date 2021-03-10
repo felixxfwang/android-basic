0093263fc8cc45ae3acfc36d416999ec927270e4
felixxfwang <felixxfwang@tencent.com>
2021-03-10 13:48:57 +0800

    opt permissions

--- utils/src/main/java/org/tiramisu/utils/DeviceUtil.kt
+++ utils/src/main/java/org/tiramisu/utils/DeviceUtil.kt
@@ -7,24 +7,35 @@ import java.util.concurrent.atomic.AtomicReference
 
 object DeviceUtil {
 
-    private var imsi = AtomicReference<String>()
-    private var mcc = AtomicReference<String>()
-    private var mnc = AtomicReference<String>()
+    private var mImsi = AtomicReference<String>()
+    private var mMcc = AtomicReference<String>()
+    private var mMnc = AtomicReference<String>()
+
+    fun mcc() = mMcc.get() ?: "NOT_READY"
+    fun mnc() = mMnc.get() ?: "NOT_READY"
 
     fun getImsi(): String {
-        return if (imsi.get().isNullOrBlank()) {
+        return if (mImsi.get().isNullOrBlank()) {
             doGetImsi()
         } else {
-            imsi.get()
+            mImsi.get()
         }
     }
 
     fun getMcc(): String {
-        return getImsi().let { if (it.length >= 3) it.substring(0, 3) else StringUtil.EMPTY }
+        return if (mMcc.get().isNullOrBlank()) {
+            getImsi().let { if (it.length >= 3) it.substring(0, 3) else StringUtil.EMPTY }
+        } else {
+            mMcc.get()
+        }
     }
 
     fun getMnc(): String {
-        return getImsi().let { if (it.length >= 5) it.substring(3, 5) else StringUtil.EMPTY }
+        return if (mMnc.get().isNullOrBlank()) {
+            getImsi().let { if (it.length >= 5) it.substring(3, 5) else StringUtil.EMPTY }
+        } else {
+            mMnc.get()
+        }
     }
 
     @Synchronized
@@ -40,7 +51,7 @@ object DeviceUtil {
     }
 
     private fun onImsiQueried(imsi: String): String {
-        this.imsi.set(imsi)
+        this.mImsi.set(imsi)
         return imsi
     }
 
