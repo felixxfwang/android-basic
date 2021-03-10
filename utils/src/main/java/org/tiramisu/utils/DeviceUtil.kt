38aa1b4ef22286d99625408c94a3ab860d27cd3a
felixxfwang <felixxfwang@tencent.com>
2021-03-10 16:20:10 +0800

    opt permissions

--- utils/src/main/java/org/tiramisu/utils/DeviceUtil.kt
+++ utils/src/main/java/org/tiramisu/utils/DeviceUtil.kt
@@ -11,42 +11,52 @@ object DeviceUtil {
     private var mMcc = AtomicReference<String>()
     private var mMnc = AtomicReference<String>()
 
+    init {
+        doGetImsi(false)
+        getMcc()
+        getMnc()
+    }
+
     fun mcc() = mMcc.get() ?: "NOT_READY"
     fun mnc() = mMnc.get() ?: "NOT_READY"
 
     fun getImsi(): String {
         return if (mImsi.get().isNullOrBlank()) {
-            doGetImsi()
+            doGetImsi(true)
         } else {
             mImsi.get()
         }
     }
 
     fun getMcc(): String {
-        return if (mMcc.get().isNullOrBlank()) {
-            getImsi().let { if (it.length >= 3) it.substring(0, 3) else StringUtil.EMPTY }
-        } else {
-            mMcc.get()
+        if (mMcc.get().isNullOrBlank()) {
+            mMcc.set(getImsi().let { if (it.length >= 3) it.substring(0, 3) else StringUtil.EMPTY })
         }
+        return mMcc.get()
     }
 
     fun getMnc(): String {
-        return if (mMnc.get().isNullOrBlank()) {
-            getImsi().let { if (it.length >= 5) it.substring(3, 5) else StringUtil.EMPTY }
-        } else {
-            mMnc.get()
+        if (mMnc.get().isNullOrBlank()) {
+            mMnc.set(getImsi().let { if (it.length >= 5) it.substring(3, 5) else StringUtil.EMPTY })
         }
+        return mMnc.get()
     }
 
     @Synchronized
-    private fun doGetImsi(): String {
-        return if (TPermissions.isPermissionGranted(permission.READ_PHONE_STATE)) {
-            onImsiQueried(PhoneUtils.getIMSI())
-        } else {
-            TPermissions.requestPermissions(permission.READ_PHONE_STATE) { granted ->
-                if (granted) onImsiQueried(PhoneUtils.getIMSI())
+    private fun doGetImsi(requestPermission: Boolean = true): String {
+        return when {
+            TPermissions.isPermissionGranted(permission.READ_PHONE_STATE) -> {
+                onImsiQueried(PhoneUtils.getIMSI())
             }
-            StringUtil.EMPTY
+            requestPermission -> {
+                TPermissions.requestPermissions(permission.READ_PHONE_STATE) { granted ->
+                    if (granted) onImsiQueried(PhoneUtils.getIMSI())
+                }
+                StringUtil.EMPTY
+            }
+            else -> {
+                StringUtil.EMPTY
+            }
         }
     }
 
