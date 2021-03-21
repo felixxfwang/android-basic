c6da39712970db466e7fbe1564ed1d8f672461cc
felixxfwang <felixxfwang@tencent.com>
2021-03-21 22:04:38 +0800

    fix report

---
+++ utils/src/main/java/org/tiramisu/utils/AppUtil.kt
@@ -0,0 +1,35 @@
+package org.tiramisu.utils
+
+import androidx.annotation.Keep
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.ProcessLifecycleOwner
+
+object AppUtil : LifecycleObserver {
+
+    interface LifecycleListener {
+        fun onForeground()
+        fun onBackground()
+    }
+
+    private val listeners = ArrayList<LifecycleListener>()
+
+    init {
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
+    }
+
+    fun registerAppLifecycleListener(listener: LifecycleListener) {
+        listeners.add(listener)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START) @Keep
+    fun onForeground() {
+        listeners.forEach { it.onForeground() }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START) @Keep
+    fun onBackground() {
+        listeners.forEach { it.onBackground() }
+    }
+}
